b6b30e4df69d1637ba22e24c0268064cff7f4f09
MechJack TV <mechjacktv@gmail.com>
2019-02-08 23:58:29 -0600

    ISSUE-1: Added functionality and unit tests to DefaultTestRandom

--- src/main/java/tv/mechjack/testframework/DefaultTestRandom.java
+++ src/main/java/tv/mechjack/testframework/DefaultTestRandom.java
@@ -1,27 +1,153 @@
 package tv.mechjack.testframework;
 
-public final class DefaultTestRandom implements TestRandom {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-  private Long nextValue = 0L;
+final class DefaultTestRandom implements TestRandom {
 
-  DefaultTestRandom() {
-    // making constructor package private
-    super();
+  private AtomicInteger nextBooleanIndex = new AtomicInteger(0);
+  private List<Boolean> nextBooleans = new ArrayList<>();
+  private AtomicInteger nextByteIndex = new AtomicInteger(0);
+  private List<Byte> nextBytes = new ArrayList<>();
+  private AtomicInteger nextByteArrayIndex = new AtomicInteger(0);
+  private List<byte[]> nextByteArrays = new ArrayList<>();
+  private AtomicInteger nextDoubleIndex = new AtomicInteger(0);
+  private List<Double> nextDoubles = new ArrayList<>();
+  private AtomicInteger nextFloatIndex = new AtomicInteger(0);
+  private List<Float> nextFloats = new ArrayList<>();
+  private AtomicInteger nextIntegerIndex = new AtomicInteger(0);
+  private List<Integer> nextIntegers = new ArrayList<>();
+  private AtomicInteger nextLongIndex = new AtomicInteger(0);
+  private List<Long> nextLongs = new ArrayList<>();
+
+  @Override
+  public boolean nextBoolean() {
+    if (nextBooleanIndex.get() >= nextBooleans.size()) {
+      throw new IllegalStateException(String.format("Boolean for index '%d' was not set.", nextBooleanIndex.get()));
+    }
+    return nextBooleans.get(nextBooleanIndex.getAndIncrement());
   }
 
   @Override
-  public final Integer nextInteger() {
-    return Integer.parseInt(this.nextValue.toString());
+  public byte nextByte() {
+    if (nextByteIndex.get() >= nextBytes.size()) {
+      throw new IllegalStateException(String.format("Byte for index '%d' was not set.", nextByteIndex.get()));
+    }
+    return nextBytes.get(nextByteIndex.getAndIncrement());
   }
 
   @Override
-  public final void setNextValue(final Long nextValue) {
-    this.nextValue = nextValue;
+  public byte[] nextByteArray() {
+    if (nextByteArrayIndex.get() >= nextByteArrays.size()) {
+      throw new IllegalStateException(String.format("ByteArray for index '%d' was not set.", nextByteArrayIndex.get()));
+    }
+    return nextByteArrays.get(nextByteArrayIndex.getAndIncrement());
+  }
+
+  @Override
+  public double nextDouble() {
+    if (nextDoubleIndex.get() >= nextDoubles.size()) {
+      throw new IllegalStateException(String.format("Double for index '%d' was not set.", nextDoubleIndex.get()));
+    }
+    return nextDoubles.get(nextDoubleIndex.getAndIncrement());
+  }
+
+  @Override
+  public float nextFloat() {
+    if (nextFloatIndex.get() >= nextFloats.size()) {
+      throw new IllegalStateException(String.format("Float for index '%d' was not set.", nextFloatIndex.get()));
+    }
+    return nextFloats.get(nextFloatIndex.getAndIncrement());
+  }
+
+  @Override
+  public int nextInteger() {
+    if (nextIntegerIndex.get() >= nextIntegers.size()) {
+      throw new IllegalStateException(String.format("Integer for index '%d' was not set.", nextIntegerIndex.get()));
+    }
+    return nextIntegers.get(nextIntegerIndex.getAndIncrement());
+  }
+
+  @Override
+  public long nextLong() {
+    if (nextLongIndex.get() >= nextLongs.size()) {
+      throw new IllegalStateException(String.format("Long for index '%d' was not set.", nextLongIndex.get()));
+    }
+    return nextLongs.get(nextLongIndex.getAndIncrement());
+  }
+
+  @Override
+  public void setNextValue(final boolean value, final boolean... values) {
+    nextBooleans.add(value);
+    for (final boolean next : values) {
+      nextBooleans.add(next);
+    }
+  }
+
+  @Override
+  public void setNextValue(final byte value, final byte... values) {
+    nextBytes.add(value);
+    for (final byte next : values) {
+      nextBytes.add(next);
+    }
+  }
+
+  @Override
+  public void setNextValue(final byte[] value, final byte[]... values) {
+    nextByteArrays.add(value);
+    nextByteArrays.addAll(Arrays.asList(values));
+  }
+
+  @Override
+  public void setNextValue(final double value, final double... values) {
+    nextDoubles.add(value);
+    for (final double next : values) {
+      nextDoubles.add(next);
+    }
+  }
+
+  @Override
+  public void setNextValue(final float value, final float... values) {
+    nextFloats.add(value);
+    for (final float next : values) {
+      nextFloats.add(next);
+    }
+  }
+
+  @Override
+  public void setNextValue(final int value, final int... values) {
+    nextIntegers.add(value);
+    for (final int next : values) {
+      nextIntegers.add(next);
+    }
+  }
+
+  @Override
+  public void setNextValue(final long value, final long... values) {
+    nextLongs.add(value);
+    for (final long next : values) {
+      nextLongs.add(next);
+    }
   }
 
   @Override
   public void reset() {
-    this.nextValue = 0L;
+    this.nextBooleanIndex.set(0);
+    this.nextBooleans.clear();
+    this.nextByteIndex.set(0);
+    this.nextBytes.clear();
+    this.nextByteArrayIndex.set(0);
+    this.nextByteArrays.clear();
+    this.nextDoubleIndex.set(0);
+    this.nextDoubles.clear();
+    this.nextFloatIndex.set(0);
+    this.nextFloats.clear();
+    this.nextIntegerIndex.set(0);
+    this.nextIntegers.clear();
+    this.nextLongIndex.set(0);
+    this.nextLongs.clear();
   }
 
 }
--- src/main/java/tv/mechjack/testframework/TestRandom.java
+++ src/main/java/tv/mechjack/testframework/TestRandom.java
@@ -1,11 +1,136 @@
 package tv.mechjack.testframework;
 
+/**
+ * Real random values and unit tests don't mix. Implementations that require
+ * random values should abstract their access behind an interface that can be
+ * faked during testing. A test implementation can wrap this class, which will
+ * automatically be reset if the test class is also using the
+ * `TestFrameworkRule`.
+ */
 public interface TestRandom {
 
-  Integer nextInteger();
+  /**
+   * Returns the next supplied `boolean` values or throws an
+   * `IllegalStateException` if no more values are available.
+   *
+   * @return the next supplied `boolean` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  boolean nextBoolean();
 
-  void setNextValue(Long nextValue);
+  /**
+   * Returns the next supplied `byte` values or throws an `IllegalStateException`
+   * if no more values are available.
+   *
+   * @return the next supplied `byte` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  byte nextByte();
 
+  /**
+   * Returns the next supplied `byte[]` values or throws an
+   * `IllegalStateException` if no more values are available.
+   *
+   * @return the next supplied `byte[]` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  byte[] nextByteArray();
+
+  /**
+   * Returns the next supplied `double` values or throws an
+   * `IllegalStateException` if no more values are available.
+   *
+   * @return the next supplied `double` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  double nextDouble();
+
+  /**
+   * Returns the next supplied `float` values or throws an `IllegalStateException`
+   * if no more values are available.
+   *
+   * @return the next supplied `float` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  float nextFloat();
+
+  /**
+   * Returns the next supplied `integer` values or throws an
+   * `IllegalStateException` if no more values are available.
+   *
+   * @return the next supplied `integer` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  int nextInteger();
+
+  /**
+   * Returns the next supplied `long` values or throws an `IllegalStateException`
+   * if no more values are available.
+   *
+   * @return the next supplied `long` value
+   * @throws IllegalStateException if no next value has been supplied
+   */
+  long nextLong();
+
+  /**
+   * Sets the next one or more `boolean` values.
+   *
+   * @param value the next `boolean` value
+   * @param values additional `boolean` values
+   */
+  void setNextValue(boolean value, boolean... values);
+
+  /**
+   * Sets the next one or more `byte` values.
+   *
+   * @param value the next `byte` value
+   * @param values additional `byte` values
+   */
+  void setNextValue(byte value, byte... values);
+
+  /**
+   * Sets the next one or more `byte[]` values.
+   *
+   * @param value the next `byte[]` value
+   * @param values additional `byte[]` values
+   */
+  void setNextValue(byte[] value, byte[]... values);
+
+  /**
+   * Sets the next one or more `double` values.
+   *
+   * @param value the next `double` value
+   * @param values additional `double` values
+   */
+  void setNextValue(double value, double... values);
+
+  /**
+   * Sets the next one or more `float` values.
+   *
+   * @param value the next `float` value
+   * @param values additional `float` values
+   */
+  void setNextValue(float value, float... values);
+
+  /**
+   * Sets the next one or more `int` values.
+   *
+   * @param value the next `int` value
+   * @param values additional `int` values
+   */
+  void setNextValue(int value, int... values);
+
+  /**
+   * Sets the next one or more `long` values.
+   *
+   * @param value the next `long` value
+   * @param values additional `long` values
+   */
+  void setNextValue(long value, long... values);
+
+  /**
+   * Resets the `TestRandom` to contain no values.
+   */
   void reset();
 
 }
